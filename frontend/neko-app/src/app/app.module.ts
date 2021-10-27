4ca1d544a243e4b0a9bca15d608709c0d6f7b4bd
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-10-27 17:20:35 +0900

    add component

--- frontend/neko-app/src/app/app.module.ts
+++ frontend/neko-app/src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MypageComponent } from './pages/mypage/mypage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    MypageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
